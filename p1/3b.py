4c69e6f1cdecefc6cabb7532c6f986950ab98755
MateusSartorio <mateussartorio39@gmail.com>
2022-11-03 07:41:51 -0300

    cool

--- p1/3b.py
+++ p1/3b.py
@@ -10,11 +10,12 @@ def dois_conjuntos(L):
             G.add_edge(L[i], L[j], sqrt((L[i][0] - L[j][0])**2 + (L[i][1] - L[j][1])**2))
 
     # Cria os conjuntos de vertices, onde cada vertice pertence a um conjunto diferente inicialmente 
-    S = DisjointSet(G.vertices())
+    S = DisjointSet(G.vertices(sort=False))
     
     # Armazena o conjunto de arestas ja ordenadas em ordem crescente de distancia
     E = G.edges(sort=True, key=lambda e: e[2])
-    
+
+    #G.show(edge_labels=True)
 
     qtd_arestas = 0
     V = len(L)
@@ -30,8 +31,11 @@ def dois_conjuntos(L):
     # 
     A = list(S)[0]
     B = list(S)[1]
+    print("Conjunto A: ", A)
+    print("Conjunto B: ", B)
 
     return A, B
 
-result = dois_conjuntos( ( (0,0), (3, 2), (1, 2.5), (2, 2), (5, 1), (1, 1) ) )
-print(result)
+
+L = ( (0,0), (3, 2), (1, 2.5), (2, 2), (5, 1), (1, 1) )
+result = dois_conjuntos(L)
--- p1/4b.py
+++ p1/4b.py
@@ -1,32 +1,70 @@
 from sage.all_cmdline import *
 
-A = ('Alg', 'Eng', 'ED1', 'ED2', 'Cal', 'Fis1', 'Fis2')
-B = ( ('Alg', 'ED2'), ('ED1', 'ED2'), ('Cal', 'ED1'), ('Fis1', 'Fis2') )
-
 def aluno_relaxado(A, B):
     G = DiGraph()
 
+    # Inicia todos os vertices (disciplinas) do grafo e atribui o valor de semestre de cada para zero
     for v in A:
         G.add_vertex(v)
         G.set_vertex(v, 0)
 
+    # Adiciona todas as dependências de disciplinas no grafo
     for e in B:
         G.add_edge(e[0], e[1])
 
-    G.show()
+    #G.show()
+
+    # Obtem o vetor de ordenacao topologica T
+    T = G.topological_sort()
+
+    # Imprime a resposta na saida padrao
+    print("Aluno relaxado: ")
+    for idx, x in enumerate(T):
+        print(f"Semestre {idx + 1}: ", x)
+
+
+def aluno_dedicado(A, B):
+    G = DiGraph()
+
+    # Inicia todos os vertices (disciplinas) do grafo e atribui o valor de semestre de cada para zero
+    for v in A:
+        G.add_vertex(v)
+        G.set_vertex(v, 0)
+
+    # Adiciona todas as dependências de disciplinas no grafo
+    for e in B:
+        G.add_edge(e[0], e[1])
+
+    #G.show()
+
+    # Obtem o vetor de ordenacao topologica T
     T = G.topological_sort()
     #print(T)
 
+    # Para cada elemento de T, percorre sua lista de adjacencia
+    # Se v aponta para um vertice u, e semestre(v) + 1 > semestre(u), atualiza-se o semestre de u para semestre(v) + 1 
     for v in T:
         for u in G.neighbors_out(v):
                     if G.get_vertex(v) + 1 > G.get_vertex(u):
                         G.set_vertex(u, G.get_vertex(v) + 1)
     
-    maior_semestre = 0
+    # Gera uma lista ordenada contendo uma lista com todas as disciplinas do primeiro semestre na primeiro posicao, todas do segundo da segunda posicao e assim por diante
     lista_ordenada = []
-    for i in G.get_vertices():
-        lista_ordenada[G.get_vertex(i)].append(1)
+    for i in G.vertices(sort=False):
+        semestre = G.get_vertex(i)
+        if semestre + 1 > len(lista_ordenada):
+            lista_ordenada.append([i])
+        else:
+            lista_ordenada[semestre].append(i)
 
+    # Imprime a lista para a saida padrao
+    print("Aluno dedicado: ")
+    for idx, x in enumerate(lista_ordenada):
+        print(f"Semestre {idx + 1}: ", x)
 
+A = ('Alg', 'Eng', 'ED1', 'ED2', 'Cal', 'Fis1', 'Fis2')
+B = ( ('Alg', 'ED2'), ('ED1', 'ED2'), ('Cal', 'ED1'), ('Fis1', 'Fis2') )
 
 aluno_relaxado(A, B)
+print()
+aluno_dedicado(A, B)
